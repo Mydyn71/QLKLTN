b121ecc990cbc3fe5a16b0006da370711c3052f6
Mydyn71 <mydyn71@gmail.com>
2025-04-10 21:52:02 +0700

    Initial commit on login branch

---
+++ US5/US4/US4/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace US4.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
